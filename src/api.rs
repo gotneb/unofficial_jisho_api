9d0641c5281cf9c127235ae2987e8fd31ceab457
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2024-06-27 18:42:11 -0400

    feat: add `kunyomi` and `onyomi` struct fields

--- src/api.rs
+++ src/api.rs
@@ -11,21 +11,57 @@ pub enum JlptLevel {
     N5,
 }
 
+#[derive(Debug)]
+pub struct YomiExample {
+    pub kanji: String,
+    pub hiragana: String,
+    pub meaning: String,
+}
+
+impl YomiExample {
+    fn new(element: &WebElement) -> Self {
+        let text = element.inner_html().unwrap();
+        let text = text.trim();
+        let mut examples: Vec<String> = text.split("\n").map(|e| String::from(e.trim())).collect();
+
+        let kanji = examples.remove(0);
+        let hiragana = examples.remove(0);
+        let hiragana = hiragana.replace("【", "").replace("】", "");
+        let meaning = examples.remove(0);
+
+        Self {
+            kanji,
+            hiragana,
+            meaning,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Kanji {
     pub taught: String,
     pub jlpt_level: JlptLevel,
     pub stroke_count: u32,
-    pub meaning: Vec<String>,
-    // kunyomi: Vec<String>,
-    // onyomi: Vec<String>,
-    // uri: String,
+    pub meaning: String,
+    pub kunyomi: Vec<String>,
+    pub kunyomi_examples: Vec<YomiExample>,
+    pub onyomi: Vec<String>,
+    pub onyomi_examples: Vec<YomiExample>,
+    pub parts: Vec<String>,
+    pub url: String,
 }
 
-pub struct API {
+pub struct JishoAPI {
     driver: GenericWebDriver<ReqwestDriverSync>,
 }
 
-impl API {
+// impl Drop for API {
+//     fn drop(&mut self) {
+//         self.driver.quit().unwrap();
+//     }
+// }
+
+impl JishoAPI {
     pub fn new() -> Self {
         let caps = DesiredCapabilities::chrome();
         let driver = WebDriver::new("http://localhost:4444", &caps).unwrap();
@@ -33,18 +69,27 @@ impl API {
     }
 
     pub fn search_kanji(&self, kanji: String) -> Kanji {
-        self.driver
-            .get(format!("{}/{} %23kanji", SCRAPE_BASE_URI, kanji))
-            .unwrap();
+        let url = format!("{}/{} %23kanji", SCRAPE_BASE_URI, kanji);
+
+        self.driver.get(&url).unwrap();
+
+        let (on_example, kun_example) = self.extract_examples();
 
         Kanji {
             taught: self.taught_in(),
             jlpt_level: self.jlpt_level(),
             stroke_count: self.stroke_count(),
             meaning: self.meaning(),
+            kunyomi: self.kunyomi(),
+            kunyomi_examples: kun_example,
+            onyomi: self.onyomi(),
+            onyomi_examples: on_example,
+            parts: self.kanji_parts(),
+            url,
         }
     }
 
+    /// Scrapes `grade`
     fn taught_in(&self) -> String {
         let elem = self
             .driver
@@ -54,6 +99,7 @@ impl API {
         elem.inner_html().unwrap()
     }
 
+    /// Scrapes `JLPT level`
     fn jlpt_level(&self) -> JlptLevel {
         let elem = self
             .driver
@@ -70,6 +116,7 @@ impl API {
         }
     }
 
+    /// Scrapes number of `strokes`
     fn stroke_count(&self) -> u32 {
         let elem = self
             .driver
@@ -79,21 +126,65 @@ impl API {
         elem.inner_html().unwrap().parse().unwrap()
     }
 
-    fn meaning(&self) -> Vec<String> {
-        todo!();
+    /// Scrapes `meaning`
+    fn meaning(&self) -> String {
+        let elem = self
+            .driver
+            .find_element(By::Css("div.kanji-details__main-meanings"))
+            .unwrap();
 
-        // Borrow checker errror
-        // let elem = self
-        //     .driver
-        //     .find_element(By::Css("div.kanji-details__main-meanings"))
-        //     .unwrap();
+        elem.inner_html().unwrap().replace("\n", "").trim().into()
+    }
 
-        // let meanings: Vec<&str> = elem
-        //     .inner_html()
-        //     .unwrap()
-        //     .replace("\n", "")
-        //     .replace(" ", "")
-        //     .split("'")
-        //     .collect();
+    /// Scrapes `kunyomi`
+    fn kunyomi(&self) -> Vec<String> {
+        self.extract_yomi("div.kanji-details__main-readings dl.dictionary_entry.kun_yomi a")
+    }
+
+    /// Scrapes `onyomi` and `kunyomi` examples. Returns in that order.
+    fn extract_examples(&self) -> (Vec<YomiExample>, Vec<YomiExample>) {
+        let elems = self
+            .driver
+            .find_elements(By::Css(
+                "div.small-12.columns div.row.compounds div.small-12.large-6.columns",
+            ))
+            .unwrap();
+
+        // There's always 2 columns
+        let onyomi_column = elems.get(0).unwrap();
+        let kunyomi_column = elems.get(1).unwrap();
+
+        // TODO: Refactor it later. I'll be ashamed if someone else see this...
+        let elems = onyomi_column
+            .find_elements(By::Css("ul.no-bullet li"))
+            .unwrap();
+        let onyomi = elems.iter().map(|e| YomiExample::new(e)).collect();
+
+        let elems = kunyomi_column
+            .find_elements(By::Css("ul.no-bullet li"))
+            .unwrap();
+        let kunyomi = elems.iter().map(|e| YomiExample::new(e)).collect();
+
+        (onyomi, kunyomi)
+    }
+
+    /// Scrapes `onyomi`
+    fn onyomi(&self) -> Vec<String> {
+        self.extract_yomi("div.kanji-details__main-readings dl.dictionary_entry.on_yomi a")
+    }
+
+    /// Scrapes parts that kanji is build up
+    fn kanji_parts(&self) -> Vec<String> {
+        self.extract_yomi("div.radicals dl.dictionary_entry.on_yomi dd a")
+    }
+
+    /// Helper function to extract `kunyomi` and `onyomi`
+    fn extract_yomi(&self, css_path: &str) -> Vec<String> {
+        let elems = self.driver.find_elements(By::Css(css_path)).unwrap();
+
+        elems
+            .iter()
+            .map(|e| String::from(e.inner_html().unwrap()))
+            .collect()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,53 @@ pub mod api;
 
 #[cfg(test)]
 mod tests {
-    use api::{JlptLevel, API};
+    use api::{JlptLevel, JishoAPI};
 
     use super::*;
 
     #[test]
-    fn search_kanji_language() {
-        let api = API::new();
+    fn search_language_kanji() {
+        let api = JishoAPI::new();
         let result = api.search_kanji("語".into());
 
         assert_eq!(result.taught, "grade 2");
         assert_eq!(result.jlpt_level, JlptLevel::N5);
         assert_eq!(result.stroke_count, 14);
-        // assert_eq!(result.meaning, "word, speech, language ");
+        assert_eq!(result.meaning, "word, speech, language");
+        assert_eq!(result.kunyomi, vec!["かた.る", "かた.らう"]);
+        assert_eq!(result.onyomi, vec!["ゴ"]);
+        assert_eq!(result.parts, vec!["口", "五", "言"]);
+
+        // Now it must test the kunyomis examples
+        // -------------------------------------------
+        assert_eq!(result.kunyomi_examples[0].kanji, "語る");
+        assert_eq!(result.kunyomi_examples[0].hiragana, "かたる");
+        assert_eq!(result.kunyomi_examples[0].meaning, "to talk about, to speak of, to tell, to narrate, to recite, to chant, to indicate, to show");
+
+        assert_eq!(result.kunyomi_examples[1].kanji, "語るに落ちる");
+        assert_eq!(result.kunyomi_examples[1].hiragana, "かたるにおちる");
+        assert_eq!(result.kunyomi_examples[1].meaning, "to let slip a secret, to let the cat out of the bag");
+
+        assert_eq!(result.kunyomi_examples[2].kanji, "語らう");
+        assert_eq!(result.kunyomi_examples[2].hiragana, "かたらう");
+        assert_eq!(result.kunyomi_examples[2].meaning, "to talk, to tell, to recite, to pledge, to conspire with");
+
+        // Now it must test the onyomis examples
+        // -------------------------------------------
+        assert_eq!(result.onyomi_examples[0].kanji, "語");
+        assert_eq!(result.onyomi_examples[0].hiragana, "ゴ");
+        assert_eq!(result.onyomi_examples[0].meaning, "word, language, speech");
+
+        assert_eq!(result.onyomi_examples[1].kanji, "語学");
+        assert_eq!(result.onyomi_examples[1].hiragana, "ゴガク");
+        assert_eq!(result.onyomi_examples[1].meaning, "study of foreign languages, linguistics");
+
+        assert_eq!(result.onyomi_examples[2].kanji, "標語");
+        assert_eq!(result.onyomi_examples[2].hiragana, "ヒョウゴ");
+        assert_eq!(result.onyomi_examples[2].meaning, "motto, slogan, catchword");
+
+        assert_eq!(result.onyomi_examples[3].kanji, "造語");
+        assert_eq!(result.onyomi_examples[3].hiragana, "ゾウゴ");
+        assert_eq!(result.onyomi_examples[3].meaning, "coinage, invention of a new word, coined word");
     }
 }
